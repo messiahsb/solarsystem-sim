5ca8ae6704f296b6789a2dc6ffd415c9326cc9e3
messiahsb <messiahs@buffalo.edu>
2026-01-23 01:21:47 -0500

    Starting setting up some files, learning pygame basics

--- planet.py
+++ planet.py
@@ -1,15 +1,15 @@
 
 class Planet:
-  def __init__(self, mass, radius):
+  def __init__(self, mass, radius, position, color):
     self.__mass = mass
     self.__radius = radius
-    self.mass = mass
-    self.setposition(position)
-    self.velocity = velocity
+    self.position = position
+    self.color = color
+    self.velocity = (0,0)
 
     def set_mass(self, mass):
       self.__mass = mass
    
 
-  def get_grade(self):
+  def get_mass(self):
     return self.__mass
